70dc9441a13ea551e1b74a37d98ec6e97f75a6d4
lbia <leonardo.bianco@outlook.com>
2020-04-25 16:41:44 +0200

    :recycle: extracted methods in ResourceUtil static class

--- src/main/java/com/leobia/gradle/JsCompiler.java
+++ src/main/java/com/leobia/gradle/JsCompiler.java
@@ -6,9 +6,7 @@ import org.gradle.api.GradleException;
 import org.gradle.api.logging.Logger;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +14,7 @@ import static com.google.javascript.jscomp.CompilerOptions.Environment.BROWSER;
 
 public class JsCompiler {
 
-    private static final String JS = "js";
+
     private final Logger logger;
 
     public JsCompiler(Logger logger) {
@@ -39,12 +37,12 @@ public class JsCompiler {
 
         Compiler compiler = new Compiler();
 
-        File output = getOutputFile(extension.getOutputPath(), extension.getInputPath());
+        File output = ResourceUtils.getOutputFile(extension.getOutputPath(), extension.getInputPath(), logger);
 
         Result result = compiler.compile(externs, inputs, options);
 
         if (result.success) {
-            write(output, compiler.toSource());
+            ResourceUtils.write(output, compiler.toSource(), logger);
         } else {
             logger.warn("Result of compilation was false");
         }
@@ -72,13 +70,13 @@ public class JsCompiler {
 
         File inputFile = new File(sourcePath);
         if (inputFile.exists()) {
-            if (isJsFile(inputFile)) {
+            if (ResourceUtils.isJsFile(inputFile)) {
                 jsSourceFiles.add(SourceFile.fromFile(inputFile.getPath()));
             } else if (inputFile.isDirectory()) {
                 File[] files = inputFile.listFiles();
                 if (files != null) {
                     for (File file : files) {
-                        if (isJsFile(file)) {
+                        if (ResourceUtils.isJsFile(file)) {
                             jsSourceFiles.add(SourceFile.fromFile(file.getPath()));
                         }
                     }
@@ -94,87 +92,4 @@ public class JsCompiler {
         return jsSourceFiles;
     }
 
-    private boolean isJsFile(File file) {
-        boolean isJs = false;
-
-        if (file != null && file.isFile()) {
-            String fileName = file.getName();
-            isJs = hasJsExtension(fileName);
-        }
-
-        return isJs;
-    }
-
-    private boolean hasJsExtension(String fileName) {
-        boolean isJs = false;
-        if (fileName.contains(".")) {
-            String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
-            isJs = JS.equals(extension.toLowerCase());
-        }
-        return isJs;
-    }
-
-    private File getOutputFile(String destinationPath, String sourcePath) {
-        File outputFile;
-
-        destinationPath = retrieveDestinationPath(destinationPath, sourcePath);
-
-        outputFile = new File(destinationPath);
-        try {
-            outputFile.createNewFile();
-        } catch (IOException e) {
-            throw new GradleException("Cannot create new file");
-        }
-
-        return outputFile;
-    }
-
-    /**
-     * Checks on destination path, if it is null it will create a new dir and new file under source path
-     * @param destinationPath destination path declared by the user
-     * @param sourcePath used as a backup if destination path is not passed
-     * @return output file
-     */
-    private String retrieveDestinationPath(String destinationPath, String sourcePath) {
-        if (destinationPath == null || destinationPath.isEmpty()) {
-            logger.warn("desitanationPath is not passed, saving files in /minified under source path");
-            File sourceFile = new File(sourcePath);
-            if (sourceFile.isFile()) {
-                destinationPath = addToPath(sourceFile.getParent(), "minified");
-            } else {
-                destinationPath = addToPath(sourcePath, "minified");
-            }
-            File minDir = new File(destinationPath);
-            minDir.mkdir();
-            destinationPath = addToPath(destinationPath, "output.js");
-        } else {
-            File destPathFile = new File(destinationPath);
-            if (destPathFile.isDirectory()) {
-                destinationPath = addToPath(destinationPath, "output.js");
-            }
-        }
-        return destinationPath;
-    }
-
-    private void write(File file, String data) {
-        file.getParentFile().mkdirs();
-        try (OutputStream stream = new FileOutputStream(file, false)) {
-            stream.write(data.getBytes());
-        } catch (IOException e) {
-            logger.error("Error writing file: " + file.getName(), e);
-        }
-    }
-
-    private String addToPath(String path, String toAdd) {
-        String output = path;
-        String lastChar = output.substring(output.length() - 1);
-
-        if (File.separator.equals(lastChar)) {
-            output += toAdd;
-        } else {
-            output += File.separator + toAdd;
-        }
-
-        return output;
-    }
 }
---
+++ src/main/java/com/leobia/gradle/ResourceUtils.java
@@ -0,0 +1,98 @@
+package com.leobia.gradle;
+
+import org.gradle.api.GradleException;
+import org.gradle.api.logging.Logger;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ResourceUtils {
+
+    public static final String JS = "js";
+
+    public static boolean isJsFile(File file) {
+        boolean isJs = false;
+
+        if (file != null && file.isFile()) {
+            String fileName = file.getName();
+            isJs = hasJsExtension(fileName);
+        }
+
+        return isJs;
+    }
+
+    public static boolean hasJsExtension(String fileName) {
+        boolean isJs = false;
+        if (fileName.contains(".")) {
+            String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+            isJs = JS.equals(extension.toLowerCase());
+        }
+        return isJs;
+    }
+
+    public static File getOutputFile(String destinationPath, String sourcePath, Logger logger) {
+        File outputFile;
+
+        destinationPath = retrieveDestinationPath(destinationPath, sourcePath, logger);
+
+        outputFile = new File(destinationPath);
+        try {
+            outputFile.createNewFile();
+        } catch (IOException e) {
+            throw new GradleException("Cannot create new file");
+        }
+
+        return outputFile;
+    }
+
+    /**
+     * Checks on destination path, if it is null it will create a new dir and new file under source path
+     * @param destinationPath destination path declared by the user
+     * @param sourcePath used as a backup if destination path is not passed
+     * @return output file
+     */
+    public static String retrieveDestinationPath(String destinationPath, String sourcePath, Logger logger) {
+        if (destinationPath == null || destinationPath.isEmpty()) {
+            logger.warn("desitanationPath is not passed, saving files in /minified under source path");
+            File sourceFile = new File(sourcePath);
+            if (sourceFile.isFile()) {
+                destinationPath = addToPath(sourceFile.getParent(), "minified");
+            } else {
+                destinationPath = addToPath(sourcePath, "minified");
+            }
+            File minDir = new File(destinationPath);
+            minDir.mkdir();
+            destinationPath = addToPath(destinationPath, "output.js");
+        } else {
+            File destPathFile = new File(destinationPath);
+            if (destPathFile.isDirectory()) {
+                destinationPath = addToPath(destinationPath, "output.js");
+            }
+        }
+        return destinationPath;
+    }
+
+    public static void write(File file, String data, Logger logger) {
+        file.getParentFile().mkdirs();
+        try (OutputStream stream = new FileOutputStream(file, false)) {
+            stream.write(data.getBytes());
+        } catch (IOException e) {
+            logger.error("Error writing file: " + file.getName(), e);
+        }
+    }
+
+    public static String addToPath(String path, String toAdd) {
+        String output = path;
+        String lastChar = output.substring(output.length() - 1);
+
+        if (File.separator.equals(lastChar)) {
+            output += toAdd;
+        } else {
+            output += File.separator + toAdd;
+        }
+
+        return output;
+    }
+}
